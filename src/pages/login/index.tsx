8f180a887775c70b36787bc30abcf81191fcef85
devluish <luis_unn@hotmail.com>
2025-04-12 22:35:39 -0300

    finalizando tela inicial

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { style } from "./style";
 import Logo from '../../assets/logo.png';
+import {MaterialIcons} from '@expo/vector-icons'
 
 import {
     Text,
     View,
     Image,
-    TextInput
+    TextInput,
+    Touchable,
+    TouchableOpacity,
+    Alert,
+    ActivityIndicator
 } from 'react-native';
+import { themas } from "../../global/themes";
 
 export default function Login() {
+    const [email,setEmail] = useState('');
+    const [password,setPassWord] = useState('');
+    const [loading,setLoading] = useState(false)
+
+    async function getLogin(){
+        try {
+            setLoading(true)
+            if(!email || !password){
+                return Alert.alert("Atenção, informe os campos obrigatorios!")
+            }
+
+            setTimeout(()=>{
+                Alert.alert("Logado com sucesso!")
+                setLoading(false)
+            },3000)
+
+            
+
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+
+
     return (
         <View style={style.container}>
             <View style={style.boxTop}>
@@ -21,14 +52,42 @@ export default function Login() {
                 <Text style={style.text}>Bem vindo!</Text>
             </View>
             <View style={style.boxMid}>
-                <Text style={style.titleInput}>Endereço de E-mail</Text>
-                <TextInput/>
-                <Text>Senha</Text>
-                <TextInput/>
+                <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
+                <View style={style.boxInput}>
+                    <TextInput
+                        style={style.input}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <MaterialIcons
+                    name='email'
+                    size={20}
+                    color={themas.colors.textColor}
+                    />
+                </View>
+                <Text style={style.titleInput}>SENHA</Text>
+                <View style={style.boxInput}>
+                    <TextInput
+                        style={style.input}
+                        value={password}
+                        onChangeText={setPassWord}
+                    />
+                    <MaterialIcons
+                    name='remove-red-eye'
+                    size={20}
+                    color={themas.colors.textColor}
+                    />
+                </View>
             </View>
             <View style={style.boxBottom}>
-                <Text>bottom</Text>
+                <TouchableOpacity style={style.button} onPress={() => getLogin()}>
+                     {loading
+                     ? <ActivityIndicator color={"#FFFF"} size={"small"} />
+                    :
+                     <Text style={style.textButton}>Entrar</Text>}
+                </TouchableOpacity>           
             </View>
-        </View>
+                <Text style={style.textRegister}>Não tem conta? <Text style={{color:themas.colors.secundary}} >Registre-se</Text></Text>
+            </View>
     );
 }
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -6,12 +6,12 @@ export const style = StyleSheet.create({
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
-        padding:20
+        padding:20,
+        backgroundColor: themas.colors.primary
     },
     boxTop:{
         height:Dimensions.get('window').height/3,
         width:'100%',
-        backgroundColor: 'red',
         alignItems: 'center',
         justifyContent: 'center'
 
@@ -20,7 +20,6 @@ export const style = StyleSheet.create({
     boxMid:{
         height:Dimensions.get('window').height/4,
         width:'100%',
-        backgroundColor: 'green',
         paddingHorizontal:37
 
     },
@@ -28,25 +27,77 @@ export const style = StyleSheet.create({
     boxBottom:{
         height:Dimensions.get('window').height/3,
         width:'100%',
-        backgroundColor: 'blue'
+        alignItems:'center',
     
-
     },
 
     logo:{
-        width:80,
-        height:80
+        width:170,
+        height:170
 
     },
 
     text:{
         fontWeight:'bold',
-        marginTop:40
+        color: 'white',
+        marginTop:40,
+        fontSize:23
 
     },
     titleInput:{
         marginLeft:5,
+        color:themas.colors.textColor,
+        marginTop:20
+
+    },
+    boxInput:{
+        width:'100%',
+        height:40,
+        borderWidth:1,
+        borderRadius:40,
+        marginTop:10,
+        flexDirection:'row', //Usando o flexD para organizar o boxInput e colocar o icon ao lado da View
+        alignItems:'center',
+        paddingHorizontal:5,
+        backgroundColor:themas.colors.white,
+        borderColor:themas.colors.textColor
+    },
+    input:{
+        height:'100%',
+        width:'90%',
+        borderRadius:40,
+        paddingLeft:5
+    },
+    button:{
+        width:250,
+        height:50,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:themas.colors.secundary,
+        borderRadius:40,
+        shadowColor: themas.colors.secundary,
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        
+        elevation: 10,
+        
+        
+    },
+    textButton:{
+        fontSize:16,
+        color:'#FFFF',
+        fontWeight:'bold'
+    },
+    textRegister:{
+        fontSize:16,
         color:themas.colors.textColor
-    }
+
+    },
+    
+    
 
 })
