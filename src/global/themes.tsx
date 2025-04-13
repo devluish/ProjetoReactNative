09353d5a89a2c0c9af040613f8b74c63a7962fd1
devluish <luis_unn@hotmail.com>
2025-04-12 21:01:04 -0300

    commit

---
+++ src/global/themes.tsx
@@ -0,0 +1,8 @@
+export const themas = {
+    colors:{
+        primary: '#171417',
+        secundary: '#B90CAA',
+        white: '#FFFFFF',
+        textColor: '#808080'
+    }
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,23 +1,34 @@
 import React from "react";
+import { style } from "./style";
+import Logo from '../../assets/logo.png';
 
 import {
     Text,
-    View
-} from 'react-native'
+    View,
+    Image,
+    TextInput
+} from 'react-native';
 
-export default function Login (){
-    return(
-        <View>
-            <View>
-
-                <View>
-                    <View>
-
-                    </View>
-                </View>
-                
+export default function Login() {
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}>
+                <Image
+                    source={Logo}
+                    style={style.logo}
+                    resizeMode="contain"
+                />
+                <Text style={style.text}>Bem vindo!</Text>
             </View>
-
+            <View style={style.boxMid}>
+                <Text style={style.titleInput}>Endereço de E-mail</Text>
+                <TextInput/>
+                <Text>Senha</Text>
+                <TextInput/>
+            </View>
+            <View style={style.boxBottom}>
+                <Text>bottom</Text>
+            </View>
         </View>
-    )
+    );
 }
